f48e2a222ee64ecb9561d649372a4a894961a98d
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-12-10 17:44:33 +0700

    get fixture files

--- utils/project-validator/src/Command/ValidateFixtureNamespaceCommand.php
+++ utils/project-validator/src/Command/ValidateFixtureNamespaceCommand.php
@@ -8,17 +8,26 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Finder\Finder;
 use Symplify\PackageBuilder\Console\ShellCode;
+use Symplify\SmartFileSystem\Finder\FinderSanitizer;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 final class ValidateFixtureNamespaceCommand extends Command
 {
+    /**
+     * @var FinderSanitizer
+     */
+    private $finderSanitizer;
+
     /**
      * @var SymfonyStyle
      */
     private $symfonyStyle;
 
-    public function __construct(SymfonyStyle $symfonyStyle)
+    public function __construct(FinderSanitizer $finderSanitizer, SymfonyStyle $symfonyStyle)
     {
+        $this->finderSanitizer = $finderSanitizer;
         $this->symfonyStyle = $symfonyStyle;
 
         parent::__construct();
@@ -31,7 +40,25 @@ final class ValidateFixtureNamespaceCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $fixtureFiles = $this->getFixtureFiles();
+        foreach ($fixtureFiles as $fixtureFile) {
+            //echo $fixtureFile . PHP_EOL;
+        }
+
         $this->symfonyStyle->success('All fixtures are correct');
         return ShellCode::SUCCESS;
+    }
+
+    private function getFixtureFiles()
+    {
+        $finder = new Finder();
+        $finder = $finder->files()
+            ->name('#\.php\.inc$#')
+            ->path('#/Fixture/#')
+            ->in(__DIR__ . '/../../../../tests')
+            ->in(__DIR__ . '/../../../../packages/*/tests')
+            ->in(__DIR__ . '/../../../../rules/*/tests');
+
+        return $this->finderSanitizer->sanitize($finder);
     }
 }
